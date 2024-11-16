99a2c3b5f82d9bc4e999d47d3fd429ad93b523f4
acoulton <andrew@ingenerator.com>
2024-11-16 13:10:00 +0000

    Initialise nullable props to null unless there's a class default

--- src/Factory/DefaultObjectFactory.php
+++ src/Factory/DefaultObjectFactory.php
@@ -4,9 +4,14 @@ declare(strict_types=1);
 namespace Ingenerator\StubObjects\Factory;
 
 use ReflectionClass;
+use ReflectionProperty;
 
 class DefaultObjectFactory
 {
+    private const int FILTER_INSTANCE_PROPERTIES = ReflectionProperty::IS_PUBLIC
+                                                   | ReflectionProperty::IS_PROTECTED
+                                                   | ReflectionProperty::IS_PRIVATE;
+
     public function __construct(
         private readonly ReflectionClass $target_reflection
     ) {
@@ -15,6 +20,8 @@ class DefaultObjectFactory
 
     public function make(array $values): object
     {
+        $values = $this->mergeDefaultValues($values);
+
         // Create the instance and apply customised values to it
         $instance = $this->target_reflection->newInstance();
         foreach ($values as $prop_name => $value) {
@@ -24,5 +31,34 @@ class DefaultObjectFactory
 
         return $instance;
     }
+
+    private function mergeDefaultValues(array $values): array
+    {
+        foreach ($this->target_reflection->getProperties(self::FILTER_INSTANCE_PROPERTIES) as $prop) {
+            $prop_name = $prop->getName();
+            if ($prop->hasDefaultValue()) {
+                // We'll just use the one defined in the class
+                continue;
+            }
+
+            if (array_key_exists($prop_name, $values)) {
+                // The caller has specified a value
+                continue;
+            }
+
+            $values[$prop_name] = $this->stubDefaultValue($prop);
+        }
+
+        return $values;
+    }
+
+    private function stubDefaultValue(ReflectionProperty $prop): mixed
+    {
+        if ($prop->getType()->allowsNull()) {
+            // If the property type is nullable, assume it can be defaulted to a null
+            return NULL;
+        }
+    }
+
 
 }
--- test/integration/DefaultPropertyValuesTest.php
+++ test/integration/DefaultPropertyValuesTest.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace test\integration\Ingenerator\StubObjects;
 
+use DateTimeImmutable;
 use Ingenerator\StubObjects\StubObjectFactory;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
@@ -42,6 +43,52 @@ class DefaultPropertyValuesTest extends TestCase
         $class = new class {
             private bool $ok = FALSE;
             private string $anything = 'new';
+
+            public function getProps()
+            {
+                return get_object_vars($this);
+            }
+        };
+
+        $result = $this->newSubject()->stub($class::class, $values);
+        $this->assertInstanceOf($class::class, $result);
+        $this->assertSame($expect, $result->getProps());
+    }
+
+    public static function provider_default_null(): array
+    {
+        $dt = new DateTimeImmutable();
+
+        return [
+            'nothing specified, all null' => [
+                [],
+                [
+                    'might_have_string' => NULL,
+                    'might_have_int' => NULL,
+                    'might_have_date' => NULL,
+                    'has_default' => 'not forced to null',
+                ],
+            ],
+            'can override' => [
+                ['might_have_date' => $dt, 'might_have_int' => 152],
+                [
+                    'might_have_string' => NULL,
+                    'might_have_int' => 152,
+                    'might_have_date' => $dt,
+                    'has_default' => 'not forced to null',
+                ],
+            ],
+        ];
+    }
+
+    #[DataProvider('provider_default_null')]
+    public function test_it_defaults_nullable_properties_to_null_even_if_no_class_default(array $values, array $expect)
+    {
+        $class = new class {
+            private ?string $might_have_string;
+            private ?int $might_have_int;
+            private ?DateTimeImmutable $might_have_date;
+            private ?string $has_default = 'not forced to null';
 
             public function getProps()
             {
